1d40dc5bb60dd558873884c6e80a80a20b75f571
Artur22 <bonjjjuaro@gmail.com>
2023-03-16 14:58:52 +0200

    Add project files.

---
+++ PdfConversion/MultiPartFormDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfConversion
+{
+    public class MultiPartFormDataModel
+    {
+        // багатокомпонентні дані форми
+        // завантажує користувач // обьект дати передачі
+        public byte[] FileUpload { get; set; }
+    }
+}
